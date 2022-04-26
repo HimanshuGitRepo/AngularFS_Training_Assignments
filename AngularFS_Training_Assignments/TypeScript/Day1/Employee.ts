77c5f5d104bccaf0cf9575e07a83e485de9dbf2a
HimanshuGitRepo <104134567+HimanshuGitRepo@users.noreply.github.com>
2022-04-26 11:21:42 +0530

    Add files via upload
    Day1 Assignment

---
+++ AngularFS_Training_Assignments/TypeScript/Day1/Employee.ts
@@ -0,0 +1,16 @@
+var Eid:number = 5464654;
+var Ename:string = "Scott";
+var Ejob:string = "Manager";
+var PerEmp: boolean = true;
+var Esalary:number = 45000;
+var Edept:number = 10;
+
+console.log("Employee Id  :" + Eid);
+console.log("Employee Name  :" + Ename);
+console.log("Employee Job  :" + Ejob);
+console.log("Is Permanent Employee  :" + PerEmp);
+console.log("Employee Salary INR :" + Esalary);
+console.log("Employee Deptno Name  :" + Edept);
+
+
+var strOutput = `Employee Id : ${Eid}, Employee Name : ${Ename}, Employee Job : ${Ejob}, Is Permanent Employee : ${PerEmp}, Employee Salary INR : ${Esalary}, Employee Deptno : ${Edept}`; 
