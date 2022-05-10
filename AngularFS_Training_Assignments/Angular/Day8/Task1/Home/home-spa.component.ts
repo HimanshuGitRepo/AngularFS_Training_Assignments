216d58ebaf52c54688812e63d106cbe362481722
HimanshuGitRepo <104134567+HimanshuGitRepo@users.noreply.github.com>
2022-05-10 11:20:54 +0530

    Add files via upload

---
+++ AngularFS_Training_Assignments/Angular/Day8/Task1/Home/home-spa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-spa',
+  templateUrl: './home-spa.component.html',
+  styleUrls: ['./home-spa.component.css']
+})
+export class HomeSPAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
