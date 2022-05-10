da30f52fb810a729ba5f30ac3b90f6a423c0f9e9
HimanshuGitRepo <104134567+HimanshuGitRepo@users.noreply.github.com>
2022-05-10 11:19:14 +0530

    Add files via upload

---
+++ AngularFS_Training_Assignments/Angular/Day8/Task1/Categories/categories-spa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-spa',
+  templateUrl: './categories-spa.component.html',
+  styleUrls: ['./categories-spa.component.css']
+})
+export class CategoriesSPAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
