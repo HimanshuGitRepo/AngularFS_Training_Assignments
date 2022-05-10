befa437da38e96ea09d890ef0fddf7546e777001
HimanshuGitRepo <104134567+HimanshuGitRepo@users.noreply.github.com>
2022-05-10 11:28:02 +0530

    Add files via upload

---
+++ AngularFS_Training_Assignments/Angular/Day8/Task1/Product/product-spa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-spa',
+  templateUrl: './product-spa.component.html',
+  styleUrls: ['./product-spa.component.css']
+})
+export class ProductSPAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
