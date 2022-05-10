47bcc2a6f99d37b25a84f3bd6c02ed3bd115fdc9
HimanshuGitRepo <104134567+HimanshuGitRepo@users.noreply.github.com>
2022-05-10 11:19:56 +0530

    Add files via upload

---
+++ AngularFS_Training_Assignments/Angular/Day8/Task1/Contact/contact-spa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-spa',
+  templateUrl: './contact-spa.component.html',
+  styleUrls: ['./contact-spa.component.css']
+})
+export class ContactSPAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
