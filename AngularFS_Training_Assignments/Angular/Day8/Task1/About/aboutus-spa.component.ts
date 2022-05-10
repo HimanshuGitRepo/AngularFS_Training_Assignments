85f15d2fa116e4f0b9c76a1544267822fe11cbf5
HimanshuGitRepo <104134567+HimanshuGitRepo@users.noreply.github.com>
2022-05-10 11:18:39 +0530

    Add files via upload

---
+++ AngularFS_Training_Assignments/Angular/Day8/Task1/About/aboutus-spa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutus-spa',
+  templateUrl: './aboutus-spa.component.html',
+  styleUrls: ['./aboutus-spa.component.css']
+})
+export class AboutusSPAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
